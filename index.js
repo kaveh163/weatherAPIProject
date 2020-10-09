248b345c9ebee28810bd97bc74a2ec586f374c20
Kaveh <kavehfrounchi@gmail.com>
2020-10-09 16:54:06 +0330

    Finished App

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ let inputValue;
 let obj;
 
 $(function () {
+    //Load weather values at start
     loadValues();
+    //Search button section
     $('#search').on('click', function (event) {
         event.preventDefault();
         inputValue = $('#inputVal').val();
@@ -11,22 +13,22 @@ $(function () {
             appid: 'ea96ca10dc430cd769f78ede39efa7a1',
             units: 'imperial'
         };
-        // getForecastData();
+
         getWeatherData();
-        // getForecastData();
+
 
     });
 
 
 });
-
+//Get Weather Api URL for Current Weather
 function getUrlApi() {
     let urlQuery = "https://api.openweathermap.org/data/2.5/weather?";
     let urlParams = $.param(obj);
     urlQuery = urlQuery + urlParams;
     return urlQuery;
 }
-
+//Get Current Weather Data from Weather Api
 function getWeatherData() {
     $.ajax({
         url: getUrlApi(),
@@ -35,7 +37,7 @@ function getWeatherData() {
             processData(data);
 
 
-            // $('#currentCond').text(data.weather[0].icon);
+
         },
         error: function (xhr, textStatus) {
             console.log(`${xhr.status} ${textStatus}`)
@@ -45,7 +47,7 @@ function getWeatherData() {
 
 let ul;
 
-// localStorage
+// localStorage Array-Obj
 let storeCurrentArray = [];
 let storeData = {};
 let currentDate;
@@ -55,11 +57,11 @@ let UVRes;
 async function processData(data) {
     console.log('first');
     currentDataRes = data;
-    // console.log(data);
+
     $('#currentCond').empty();
     // print city and date
     let dt = data.dt;
-    // console.log(dt);
+
     let date = new Date(dt * 1000);
     let city = '<span id="city">' + data.name + ' ' + '(' + date.toLocaleDateString() + ')' + '</span>';
     currentDate = date.toLocaleDateString();
@@ -68,7 +70,7 @@ async function processData(data) {
     const imgWrapper = $('<div>');
     imgWrapper.append(city);
 
-    // console.log(date.toLocaleDateString());
+
     const img = $('<img src="" alt="weatherCondition">');
     let iconCode = data.weather[0].icon;
     let iconUrl = "http://openweathermap.org/img/wn/" + iconCode + ".png";
@@ -84,39 +86,34 @@ async function processData(data) {
 
     $('#currentCond').append(ul);
 
-    // let Temp = $('<li>').text(`Temperature: ${data.main.temp} ${&#8457;}`);
+
     let Temp = $('<li>');
-    // Temp.attr('value', '' + String.fromCharCode(176));
-    // Temp.text($(Temp).attr('value'));
-    // Temp.append('&deg;');
+
     Temp.append(`Temperature: ${data.main.temp}&#8457;`);
     ul.append(Temp);
+    //Print Humidity Conditions
     let Humidity = $('<li>');
     Humidity.text('Humidity: ' + data.main.humidity + '%');
     ul.append(Humidity);
+    //Print Wind Speed Conditions
     let windSpeed = $('<li>');
     windSpeed.text('Wind Speed: ' + data.wind.speed + 'MPH');
     ul.append(windSpeed);
-    // console.log(data.coord.lat);
+    //Get latitude and longitude
     let latitude = data.coord.lat;
-    // console.log(data.coord.lon);
+
     let longitude = data.coord.lon;
+    //Get UV Result
     let UVResult = await getUVIndex(latitude, longitude);
     console.log(UVResult);
-    UVIndex= getUVData(UVResult);
+    //Print UV Result
+    UVIndex = getUVData(UVResult);
     getForecastData();
-    //to local storage
-
-
-    //UV Index
-
 
 
 }
 
-// let urlQuery = "api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-// let urlQuery = "https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=YOUR_API_KEY";
-// let urlQuery = "https://api.openweathermap.org/data/2.5/weather?";
+
 let UVIndex;
 
 
@@ -125,66 +122,18 @@ async function getUVIndex(lat, lon) {
     try {
         res = await $.ajax({
             url: `http://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=ea96ca10dc430cd769f78ede39efa7a1`,
-            // url: `http://api.openweathermap.org/v3/uvi/40.7,-74.2/current.json?appid=ea96ca10dc430cd769f78ede39efa7a1`,
+
             method: 'GET'
         });
-    } catch(error) {
+    } catch (error) {
         console.log(error);
     }
     return res;
 
 }
-        // success: function (res) {
-            // console.log(res.value);
-            // console.log('second');
-            // getUVData(res);
 
-            // UVRes = res;
-            // UVIndex = res.value;
-            // console.log(UVIndex);
-            // let UVIndexVal = $('<li>');
-            // UVIndexVal.append('UV Index: ' + '<span id="UVData">' + UVIndex + '</span>');
-            // ul.append(UVIndexVal);
-
-            //    to local storage
-            //     storeData['currUV'] = UVIndex;
-
-            // console.log('result',resultRes);
-
-            // let getStoreWeather = getCurrWXStorage();
-            // if(getStoreWeather === null) {
-            //     getStoreWeather = [];
-            //     storeData['city'] = data.name;
-            //     storeData['currDate'] = currentDate;
-            //     storeData['currIcon'] = data.weather[0].icon;
-            //     storeData['currTemp'] = data.main.temp;
-            //     storeData['currHumidity'] = data.main.humidity;
-            //     storeData['currWS'] = data.wind.speed;
-            //     // store future data
-            //     storeData['futureData'] = {};
-            //
-            //     getStoreWeather.push(storeData);
-            //     setCurrWXStorage(getStoreWeather);
-            // } else {
-            //     storeData['city'] = data.name;
-            //     storeData['currDate'] = currentDate;
-            //     storeData['currIcon'] = data.weather[0].icon;
-            //     storeData['currTemp'] = data.main.temp;
-            //     storeData['currHumidity'] = data.main.humidity;
-            //     storeData['currWS'] = data.wind.speed;
-            //     getStoreWeather.push(storeData);
-            //     setCurrWXStorage(getStoreWeather);
-            // }
-
-        // },
-        // error: function (xhr, textStatus) {
-        //     console.log(`${xhr.status} ${textStatus}`);
-        // }
-
-
-
-
-function getUVData(res){
+//Print UV Result
+function getUVData(res) {
     UVRes = res;
     UVIndex = res.value;
     console.log('second', UVIndex);
@@ -193,8 +142,8 @@ function getUVData(res){
     ul.append(UVIndexVal);
     return UVIndex;
 }
-// setCurrWXStorage();
 
+//Get Weather Api forecast Url
 function getForecastUrlApi() {
     let urlForecastQuery = "https://api.openweathermap.org/data/2.5/forecast?";
     let urlParams = $.param(obj);
@@ -207,7 +156,7 @@ function getForecastData() {
         url: getForecastUrlApi(),
         method: 'GET',
         success: function (result) {
-            // console.log(result);
+
             processForecastData(result);
         },
         error: function (xhr, textStatus) {
@@ -218,6 +167,7 @@ function getForecastData() {
 
 let futureDataRes;
 let listDataLength;
+
 function processForecastData(result) {
     console.log('third');
     $('#futureInfo').empty();
@@ -230,7 +180,7 @@ function processForecastData(result) {
         let minutes = forecastDate.getMinutes();
         let seconds = forecastDate.getSeconds();
         if (hour === 0 && minutes === 0 && seconds === 0) {
-
+            //populate 5 weather Forecast
             let cards = $('<div>').addClass('card bg-primary');
             $('#futureInfo').append(cards);
             let cardBody = $('<div>').addClass('card-body');
@@ -239,7 +189,7 @@ function processForecastData(result) {
             let forecastDay = forecastDate.toLocaleDateString();
             futureDate.text(forecastDay);
             cardBody.append(futureDate);
-            // console.log(forecastDay);
+
 
             let weatherIcon = $('<img src="" alt="WeatherIcon">');
             let weatherIconCode = result.list[i].weather[0].icon;
@@ -251,42 +201,42 @@ function processForecastData(result) {
             let tempForecast = result.list[i].main.temp;
             temperature.append(`Temp: ${tempForecast} &#8457;`);
             cardBody.append(temperature);
-            // console.log(tempForecast);
+
 
             let humidityCond = $('<p>').addClass('card-text');
             let humidityForecast = result.list[i].main.humidity;
             humidityCond.text(`Humidity: ${humidityForecast}%`);
             cardBody.append(humidityCond);
-            // console.log(humidityForecast);
+
         }
     }
-    console.log('future', futureDataRes);
-    console.log('current', currentDataRes);
+
     handleLocalStorage();
     populateSearch();
 }
-function populateSearch(){
+
+function populateSearch() {
     let weatherArray = getWXStorage();
 
     let searchHistory = $('#searchHistory');
     searchHistory.empty();
     let searchList = $('<ul>').addClass('list-group');
     searchHistory.append(searchList);
-    for(let i=0; i < weatherArray.length; i++){
+    for (let i = 0; i < weatherArray.length; i++) {
         let weatherCity = weatherArray[i].city;
         let searchItem = $('<li>').addClass('list-group-item');
         searchList.append(searchItem);
-        let listBtn = $('<a class="btn">').attr({'href':'#', 'data-city': weatherCity}).text(weatherCity);
+        let listBtn = $('<a class="btn">').attr({'href': '#', 'data-city': weatherCity}).text(weatherCity);
         searchItem.append(listBtn)
     }
 }
+
 function setWXStorage(arr) {
-    // arr.push(storeData);
-    console.log('array1', arr);
+
+
     localStorage.setItem('weatherData', JSON.stringify(arr));
 }
 
-console.log(storeData);
 
 function getWXStorage() {
     return JSON.parse(localStorage.getItem('weatherData'));
@@ -300,10 +250,10 @@ function handleLocalStorage() {
         getWeatherArray.push(storeData);
         setWXStorage(getWeatherArray);
     } else {
-        for(let i=0; i<getWeatherArray.length; i++){
-            if(getWeatherArray[i].city === currentDataRes.name){
+        for (let i = 0; i < getWeatherArray.length; i++) {
+            if (getWeatherArray[i].city === currentDataRes.name) {
                 storeWeatherData();
-                getWeatherArray.splice(i,1,storeData);
+                getWeatherArray.splice(i, 1, storeData);
                 setWXStorage(getWeatherArray);
                 return;
             }
@@ -322,11 +272,10 @@ function storeWeatherData() {
     storeData['currTemp'] = currentDataRes.main.temp;
     storeData['currHumidity'] = currentDataRes.main.humidity;
     storeData['currWS'] = currentDataRes.wind.speed;
-    console.log('UV',UVIndex);
-    storeData['currUV']= UVIndex;
-    storeData['futureWeatherArray']= [];
-    // storeData['futureWeather']= {};
-    // let futureWeather= {};
+    console.log('UV', UVIndex);
+    storeData['currUV'] = UVIndex;
+    storeData['futureWeatherArray'] = [];
+
 
     // For Loop
     for (let i = 0; i < listDataLength; i++) {
@@ -336,49 +285,48 @@ function storeWeatherData() {
         let minutes = forecastDate.getMinutes();
         let seconds = forecastDate.getSeconds();
         if (hour === 0 && minutes === 0 && seconds === 0) {
-            let futureWeather= futureWX();
-            function futureWX(){
+            let futureWeather = futureWX();
+
+            function futureWX() {
                 return {
                     'futDayPred': '',
                     'futIconCode': '',
                     'futTemp': 0,
-                    'futHumidity':0
+                    'futHumidity': 0
                 }
             }
+
             let forecastDay = forecastDate.toLocaleDateString();
             futureWeather['futDayPred'] = forecastDay;
-            // console.log('forecast',forecastDay);
+
 
             let weatherIconCode = futureDataRes.list[i].weather[0].icon;
             futureWeather['futIconCode'] = weatherIconCode;
-            // console.log('icon',weatherIconCode);
-
 
 
             let tempForecast = futureDataRes.list[i].main.temp;
             futureWeather['futTemp'] = tempForecast;
 
-            // console.log('temperature',tempForecast);
-
 
             let humidityForecast = futureDataRes.list[i].main.humidity;
             futureWeather['futHumidity'] = humidityForecast;
 
-            // storeData['futureWeatherArray']= [{...futureWeather}];
+
             storeData['futureWeatherArray'].push(futureWeather);
-            // console.log('humidity', humidityForecast);
+
         }
     }
 
 }
-$(document).on('click', 'li .btn', function(event){
+// Handle Search History Cities
+$(document).on('click', 'li .btn', function (event) {
     event.preventDefault();
     let dataCity = $(this).attr('data-city');
-    // console.log(dataCity);
+
     let getWeather = getWXStorage();
-    console.log('getWeather', getWeather);
-    for(let i=0; i< getWeather.length; i++){
-        if(getWeather[i].city === dataCity) {
+
+    for (let i = 0; i < getWeather.length; i++) {
+        if (getWeather[i].city === dataCity) {
 
             $('#currentCond').empty();
             // print city and date
@@ -390,7 +338,7 @@ $(document).on('click', 'li .btn', function(event){
             let Wrapper = $('<div>');
             Wrapper.append(city);
 
-            // console.log(date.toLocaleDateString());
+
             let img = $('<img src="" alt="weatherCondition">');
             let iconCode = getWeather[i].currIcon;
             let iconUrl = "http://openweathermap.org/img/wn/" + iconCode + ".png";
@@ -406,11 +354,9 @@ $(document).on('click', 'li .btn', function(event){
 
             $('#currentCond').append(ul);
 
-            // let Temp = $('<li>').text(`Temperature: ${data.main.temp} ${&#8457;}`);
+
             let Temp = $('<li>');
-            // Temp.attr('value', '' + String.fromCharCode(176));
-            // Temp.text($(Temp).attr('value'));
-            // Temp.append('&deg;');
+
             Temp.append(`Temperature: ${getWeather[i].currTemp}&#8457;`);
             ul.append(Temp);
             let Humidity = $('<li>');
@@ -419,24 +365,22 @@ $(document).on('click', 'li .btn', function(event){
             let windSpeed = $('<li>');
             windSpeed.text('Wind Speed: ' + getWeather[i].currWS + 'MPH');
             ul.append(windSpeed);
-            // console.log(data.coord.lat);
+
             let UVIndexVal = $('<li>');
             UVIndexVal.append('UV Index: ' + '<span id="UVData">' + getWeather[i].currUV + '</span>');
             ul.append(UVIndexVal);
 
 
-        // populate five day forecast
+            // populate five day forecast
             $('#futureInfo').empty();
             let futureWXArr = getWeather[i].futureWeatherArray;
-            let futureWXDay = getWeather[i].futureWeatherArray[0].futDayPred;
-            console.log('5',futureWXDay);
-            console.log(futureWXArr);
-            console.log(futureWXArr.length);
-            for(let j=0; j < getWeather[i].futureWeatherArray.length; j++){
-                let futDay =  getWeather[i].futureWeatherArray[j].futDayPred;
-                let futIconCode =  getWeather[i].futureWeatherArray[j].futIconCode;
-                let futTemp =  getWeather[i].futureWeatherArray[j].futTemp;
-                let futHumidity =  getWeather[i].futureWeatherArray[j].futHumidity;
+
+
+            for (let j = 0; j < getWeather[i].futureWeatherArray.length; j++) {
+                let futDay = getWeather[i].futureWeatherArray[j].futDayPred;
+                let futIconCode = getWeather[i].futureWeatherArray[j].futIconCode;
+                let futTemp = getWeather[i].futureWeatherArray[j].futTemp;
+                let futHumidity = getWeather[i].futureWeatherArray[j].futHumidity;
 
 
                 let cards = $('<div>').addClass('card bg-primary');
@@ -447,7 +391,7 @@ $(document).on('click', 'li .btn', function(event){
                 let forecastDay = futDay;
                 futureDate.text(forecastDay);
                 cardBody.append(futureDate);
-                // console.log(forecastDay);
+
 
                 let weatherIcon = $('<img src="" alt="WeatherIcon">');
                 let weatherIconCode = futIconCode;
@@ -459,13 +403,12 @@ $(document).on('click', 'li .btn', function(event){
                 let tempForecast = futTemp;
                 temperature.append(`Temp: ${tempForecast} &#8457;`);
                 cardBody.append(temperature);
-                // console.log(tempForecast);
+
 
                 let humidityCond = $('<p>').addClass('card-text');
                 let humidityForecast = futHumidity;
                 humidityCond.text(`Humidity: ${humidityForecast}%`);
                 cardBody.append(humidityCond);
-
 
 
             }
@@ -476,11 +419,12 @@ $(document).on('click', 'li .btn', function(event){
 
 });
 
-function loadValues(){
+//Load Values when refresh
+function loadValues() {
     let getWeather = getWXStorage();
-    if(getWeather != null) {
+    if (getWeather != null) {
         let lastSearch = getWeather.pop();
-        console.log(lastSearch);
+
         $('#currentCond').empty();
 
         //populate load values
@@ -491,7 +435,7 @@ function loadValues(){
         let Wrapper = $('<div>');
         Wrapper.append(city);
 
-        // console.log(date.toLocaleDateString());
+
         let img = $('<img src="" alt="weatherCondition">');
         let iconCode = lastSearch.currIcon;
         let iconUrl = "http://openweathermap.org/img/wn/" + iconCode + ".png";
@@ -507,32 +451,32 @@ function loadValues(){
 
         $('#currentCond').append(ul);
 
-        // let Temp = $('<li>').text(`Temperature: ${data.main.temp} ${&#8457;}`);
+
         let Temp = $('<li>');
-        // Temp.attr('value', '' + String.fromCharCode(176));
-        // Temp.text($(Temp).attr('value'));
-        // Temp.append('&deg;');
+
         Temp.append(`Temperature: ${lastSearch.currTemp}&#8457;`);
         ul.append(Temp);
+
+        //print Humidity Conditions
         let Humidity = $('<li>');
         Humidity.text('Humidity: ' + lastSearch.currHumidity + '%');
         ul.append(Humidity);
+        //print windSpeed Conditions
         let windSpeed = $('<li>');
         windSpeed.text('Wind Speed: ' + lastSearch.currWS + 'MPH');
         ul.append(windSpeed);
-        // console.log(data.coord.lat);
+        //print UV Value
         let UVIndexVal = $('<li>');
         UVIndexVal.append('UV Index: ' + '<span id="UVData">' + lastSearch.currUV + '</span>');
         ul.append(UVIndexVal);
 
 
         // populate five day forecast
+
         $('#futureInfo').empty();
         let futureWXArr = lastSearch.futureWeatherArray;
-        let futureWXDay = lastSearch.futureWeatherArray[0].futDayPred;
-        console.log('5', futureWXDay);
-        console.log(futureWXArr);
-        console.log(futureWXArr.length);
+
+
         for (let j = 0; j < lastSearch.futureWeatherArray.length; j++) {
             let futDay = lastSearch.futureWeatherArray[j].futDayPred;
             let futIconCode = lastSearch.futureWeatherArray[j].futIconCode;
@@ -548,7 +492,7 @@ function loadValues(){
             let forecastDay = futDay;
             futureDate.text(forecastDay);
             cardBody.append(futureDate);
-            // console.log(forecastDay);
+
 
             let weatherIcon = $('<img src="" alt="WeatherIcon">');
             let weatherIconCode = futIconCode;
@@ -560,7 +504,7 @@ function loadValues(){
             let tempForecast = futTemp;
             temperature.append(`Temp: ${tempForecast} &#8457;`);
             cardBody.append(temperature);
-            // console.log(tempForecast);
+
 
             let humidityCond = $('<p>').addClass('card-text');
             let humidityForecast = futHumidity;
